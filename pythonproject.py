61ce7249da2ae07b1293902755c6d68efa89eeab
N.Shah <nns70@scarletmail.rutgers.edu>
2018-11-01 16:12:38 -0400

    Update pythonproject.py

--- pythonproject.py
+++ pythonproject.py
@@ -22,3 +22,16 @@ print "            RRRRRRRRRRRR"
 #need it to print the information
 #the unicodes make up the ears
 #Need to use randomizer to get the information to randomize
+import random
+nouns = ("")
+verbs = ("")
+adv = ("")
+adj = ("")
+print nouns[num] + ' ' + verbs[num] + ' ' + adv[num] + ' ' + adj[num]
+  l=[nouns,verbs,adj,adv]
+ ' '.join([random.choice(i) for i in l])
+' '.join([random.choice(i) for i in l])
+    idx = random.randrange(1, 10)
+import random
+print("%s %s %s %s"%(random.choice(nouns), random.choice(verbs), random.choice(adv), random.choice(adj))
+#Used to randomize facts by choosing random animals and adjectives that express the lengths of animal cruelty in regards to animal testing
