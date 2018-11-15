9983c40d7900bc00aaa02d62ea0360da89f6c407
N.Shah <nns70@scarletmail.rutgers.edu>
2018-11-15 14:14:11 -0500

    updated

--- pythonproject.py
+++ pythonproject.py
@@ -4,20 +4,7 @@ print u"   \U0001F407\U0001F407\U0001F407\U0001F407                      \U0001F
 print u"   \U0001F484\U0001F484\U0001F484\U0001F484                      \U0001F484\U0001F484\U0001F484\U0001F484"
 print u"   \U0001F407\U0001F407\U0001F407\U0001F407                      \U0001F407\U0001F407\U0001F407\U0001F407"
 print u"   \U0001F484\U0001F484\U0001F484\U0001F484                      \U0001F484\U0001F484\U0001F484\U0001F484"
-print "    RRR RRRRRRRRRRRRRRRRRRRR RRR"
-print "    RRRRRRRRRRRRRRRRRRRRRRRRRRRR "
-print "   RRRRRRRRRRRRRRRRRRRRRRRRRRRRRR"
-print "  RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR"
-print "  RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR"
-print "  RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR"
-print "  RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR"
-print "   RRRRRRRRRRRRRRRRRRRRRRRRRRRRRR"
-print "    RRRRRRRRRRRRRRRRRRRRRRRRRRRR"
-print "     RRRRRRRRRRRRRRRRRRRRRRRRRR"
-print "      RRRRRRRRRRRRRRRRRRRRRRRR"
-print "       RRRRRRRRRRRRRRRRRRRRRR"
-print "        RRRRRRRRRRRRRRRRRRRR"
-print "            RRRRRRRRRRRR"
+#Neeed to get the below code to get in the form of a bunny
 #Import the text file so it will randomize facts into the shape of the bunny above in order to show the impact of animal cruelty in regards to animal testing
 import random
 f = open('makeup.txt','r')#to open the text file and the next line is to read it and split the lines
