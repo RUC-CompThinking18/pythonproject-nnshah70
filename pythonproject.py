4372c4eee72e128612ebeb2727ac82c5c731992a
N.Shah <nns70@scarletmail.rutgers.edu>
2018-11-15 13:54:39 -0500

    Update pythonproject.py

--- pythonproject.py
+++ pythonproject.py
@@ -1,4 +1,4 @@
-print u"    \U0001F407                         \U0001F407"#Unicodes for the ears, bunny and lipsticks for animal testing 
+print u"    \U0001F407                         \U0001F407"#Unicodes for the ears, bunny and lipsticks for animal testing
 print u"   \U0001F484\U0001F484\U0001F484                       \U0001F484\U0001F484\U0001F484"
 print u"   \U0001F407\U0001F407\U0001F407\U0001F407                      \U0001F407\U0001F407\U0001F407\U0001F407"
 print u"   \U0001F484\U0001F484\U0001F484\U0001F484                      \U0001F484\U0001F484\U0001F484\U0001F484"
@@ -18,11 +18,11 @@ print "      RRRRRRRRRRRRRRRRRRRRRRRR"
 print "       RRRRRRRRRRRRRRRRRRRRRR"
 print "        RRRRRRRRRRRRRRRRRRRR"
 print "            RRRRRRRRRRRR"
-#Import the text file so it will randomize facts into the shape of the bunny above
+#Import the text file so it will randomize facts into the shape of the bunny above in order to show the impact of animal cruelty in regards to animal testing
 import random
 f = open('makeup.txt','r')#to open the text file and the next line is to read it and split the lines
 file  = f.read().split('\n')
-
+#Every time someone runs the code, each time the facts will change but the shape will stay the same
 random.shuffle(file)#So that the facts will change
 facts = file[0].split(' ')
 #To join the facts in the shape of the bunny
